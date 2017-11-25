f46d20654534be6906d471150a67e77fefd348b2
jeremymarch <jmarch@gradcenter.cuny.edu>
2017-11-25 14:53:25 -0500

    First step of syncing json vocab from server to core data is working.

--- hctest/AppDelegate.swift
+++ hctest/AppDelegate.swift
@@ -11,6 +11,7 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    let appName = "hctest"
     let dbname:String = "hcdatadb.sqlite"
     let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
     let dbpath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "/" + "hcdatadb.sqlite"
@@ -29,10 +30,83 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             return UIInterfaceOrientationMask.all
         }
     }
+    
+    struct HQResponse : Codable {
+        struct Meta : Codable {
+            let updated: Int
+            enum CodingKeys : String, CodingKey {
+                case updated = "updated"
+            }
+        }
+        struct Row : Codable {
+            let id: Int
+            let lemma: String
+            let unit: Int
+        }
+        let meta: Meta
+        let rows: [Row]
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        datasync()
         return true
+    }
+    
+    func datasync()
+    {
+        NSLog("START REQUEST")
+        //http://benscheirman.com/2017/06/ultimate-guide-to-json-parsing-with-swift-4/
+        //https://stackoverflow.com/questions/32631184/the-resource-could-not-be-loaded-because-the-app-transport-security-policy-requi
+        let urlString = URL(string: "http://philolog.us/hqjson.php")//http://philolog.us/hqvocab.php?unit=20&AndUnder=on&sort=alpha")
+        if let url = urlString {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    NSLog("boo")
+                    NSLog(error!.localizedDescription)
+                } else {
+                    if let usableData = data {
+                        NSLog("yay")
+                        if let stringData = String(data: usableData, encoding: String.Encoding.utf8) {
+                            print(stringData) //JSONSerialization
+                            do {
+                                let decoder = JSONDecoder()
+                                let rows = try decoder.decode(HQResponse.self, from: usableData)
+                                NSLog("Updated: \(rows.meta.updated)")
+                                for row in rows.rows {
+                                    print("Row: \(row.id), \(row.lemma), \(row.unit)")
+
+                                    //https://stackoverflow.com/questions/46956921/main-thread-checker-ui-api-called-on-a-background-thread-uiapplication-deleg
+                                    let backgroundContext = NSManagedObjectContext(concurrencyType:.privateQueueConcurrencyType)
+                                    if #available(iOS 10.0, *) {
+                                        backgroundContext.persistentStoreCoordinator = self.persistentContainer.persistentStoreCoordinator
+                                    }
+                                    else
+                                    {
+                                        backgroundContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+                                    }
+                                    let entity = NSEntityDescription.entity(forEntityName: "Words", in: backgroundContext)
+                                    let newUser = NSManagedObject(entity: entity!, insertInto: backgroundContext)
+                                    newUser.setValue(row.id, forKey: "hqid")
+                                    newUser.setValue(row.unit, forKey: "unit")
+                                    newUser.setValue(row.lemma, forKey: "lemma")
+                                    do {
+                                        try backgroundContext.save()
+                                    } catch {
+                                        print("Failed saving")
+                                    }
+                                }
+                            } catch let error as NSError {
+                                print(error.localizedDescription)
+                            }
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+        NSLog("End REQUEST")
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -60,49 +134,183 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: - Core Data stack
-/*
+    //https://stackoverflow.com/questions/22582020/crash-when-using-nsreadonlypersistentstoreoption
+    @available(iOS 10.0, *)
     lazy var persistentContainer: NSPersistentContainer = {
+        let appName = "hctest"
+        
+        let container = NSPersistentContainer(name: appName)
+        let usePreloadedStore = false
+        if (usePreloadedStore)
+        {
+        let seededData: String = appName
+        var persistentStoreDescriptions: NSPersistentStoreDescription
+        
+        //let storeUrl = self.applicationDocumentsDirectory.appendingPathComponent("app_name.sqlite")
+        //let storeURL = [[NSBundle mainBundle] URLForResource:@"philologus" withExtension:@"sqlite"];
+        let storeURL = Bundle.main.url(forResource: appName, withExtension: "sqlite")
         /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "hctest")
+         if !FileManager.default.fileExists(atPath: (storeURL?.path)!) {
+         let seededDataUrl = Bundle.main.url(forResource: seededData, withExtension: "sqlite")
+         try! FileManager.default.copyItem(at: seededDataUrl!, to: storeURL!)
+         
+         }
+         */
+        print(storeURL!)
+        //var options = NSMutableDictionary()
+        //options[NSReadOnlyPersistentStoreOption] = true
+        
+        //container.persistentStoreCoordinator.addPersistentStore(ofType: , configurationName: , at: , options: )
+        
+        let d:NSPersistentStoreDescription = NSPersistentStoreDescription(url: storeURL!)
+        d.setOption(true as NSObject, forKey: NSReadOnlyPersistentStoreOption)
+        d.setOption(["journal_mode": "delete"] as NSObject!, forKey: NSSQLitePragmasOption)
+        container.persistentStoreDescriptions = [d]
+        
+        //persistentStoreDescriptions.setOption(true as NSObject, forKey: NSReadOnlyPersistentStoreOption)
+        //container.persistentStoreCoordinator.
+        }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+            if let error = error {
+                
+                fatalError("Unresolved error \(error),")
             }
         })
+        
         return container
     }()
-*/
+    
+    // iOS 9 and below
+    lazy var applicationDocumentsDirectory: URL = {
+        
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return urls[urls.count-1]
+    }()
+    
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+        let modelURL = Bundle.main.url(forResource: "hctest", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelURL)!
+    }()
+    
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+        // Create the coordinator and store
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        //let url = self.applicationDocumentsDirectory.appendingPathComponent("philolog_us.sqlite")
+        let url = Bundle.main.url(forResource: "hctest", withExtension: "sqlite")!
+        var failureReason = "There was an error creating or loading the application's saved data."
+        
+        let opt = [ NSReadOnlyPersistentStoreOption: true as NSObject,
+                    NSSQLitePragmasOption: ["journal_mode": "delete"] as NSObject!,
+                    NSMigratePersistentStoresAutomaticallyOption:false as NSObject,
+                    NSInferMappingModelAutomaticallyOption:false as NSObject]
+        
+        do {
+            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: opt as Any as? [AnyHashable : Any])
+        } catch {
+            // Report any error we got.
+            var dict = [String: AnyObject]()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data" as AnyObject?
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason as AnyObject?
+            
+            dict[NSUnderlyingErrorKey] = error as NSError
+            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            // Replace this with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+            abort()
+        }
+        
+        return coordinator
+    }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext = {
+        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+        let coordinator = self.persistentStoreCoordinator
+        var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }()
+    
+    
+    /*
+     lazy var persistentContainer: NSPersistentContainer = {
+     /*
+     The persistent container for the application. This implementation
+     creates and returns a container, having loaded the store for the
+     application to it. This property is optional since there are legitimate
+     error conditions that could cause the creation of the store to fail.
+     */
+     let container = NSPersistentContainer(name: "philolog_us")
+     //container.persistentStoreDescriptions[0].setOption(, forKey: )
+     container.persistentStoreDescriptions[0].setOption(true as NSObject, forKey: NSReadOnlyPersistentStoreOption)
+     container.persistentStoreDescriptions[0].setOption(["journal_mode": "delete"] as NSObject!, forKey: NSSQLitePragmasOption)
+     container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+     if let error = error as NSError? {
+     // Replace this implementation with code to handle the error appropriately.
+     // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+     
+     /*
+     Typical reasons for an error here include:
+     * The parent directory does not exist, cannot be created, or disallows writing.
+     * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+     * The device is out of space.
+     * The store could not be migrated to the current model version.
+     Check the error message to determine what the actual problem was.
+     */
+     fatalError("Unresolved error \(error), \(error.userInfo)")
+     }
+     })
+     return container
+     }()
+     */
     // MARK: - Core Data Saving support
-/*
+    
     func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        
+        if #available(iOS 10.0, *) {
+            
+            let context = persistentContainer.viewContext
+            if context.hasChanges {
+                do {
+                    try context.save()
+                } catch {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    let nserror = error as NSError
+                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                }
+                
+            } else {
+                // iOS 9.0 and below - however you were previously handling it
+                if managedObjectContext.hasChanges {
+                    do {
+                        try managedObjectContext.save()
+                    } catch {
+                        // Replace this implementation with code to handle the error appropriately.
+                        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                        let nserror = error as NSError
+                        NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+                        abort()
+                    }
+                }
             }
         }
     }
-    */
+}
+
+@available(iOS 10.0, *)
+extension NSPersistentContainer {
+    
+    public convenience init(name: String, bundle: Bundle) {
+        guard let modelURL = bundle.url(forResource: name, withExtension: "momd"),
+            let mom = NSManagedObjectModel(contentsOf: modelURL)
+            else {
+                fatalError("Unable to located Core Data model")
+        }
+        
+        self.init(name: name, managedObjectModel: mom)
+    }
 }
 
