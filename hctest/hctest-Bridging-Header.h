d6f1e384e0b012b9e14419bb826690ab4cc054eb
Jeremy <jeremy@Jeremys-MacBook.local>
2019-03-03 19:25:03 -0500

    now builds with new submodules

--- hctest/hctest-Bridging-Header.h
+++ hctest/hctest-Bridging-Header.h
@@ -2,9 +2,9 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "libmorph.h"
-#import "GreekForms.h"
-#import "VerbSequence.h"
-#import "accent.h"
-#import "sqlite3.h"
-#import "swiftVSeqLayer.h"
+#import "libgreek/libmorph.h"
+#import "libgreek/GreekForms.h"
+#import "libseq/VerbSequence.h"
+#import "libgreek/accent.h"
+#import "libseq/sqlite3.h"
+#import "libseq/swiftVSeqLayer.h"
